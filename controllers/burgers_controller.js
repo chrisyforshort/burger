ad3cf09cd4458278847138047ea503214862b586
chrisyshorshort <chritinasarahwu@gmail.com>
2018-05-02 18:32:43 -0700

    handlebar

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+router.get('/burgers', function(req,res){
+    burger.all(function(data){
+        var hbsObject = {burgers: data};
+        res.render('test', hbsObject);
+    })
+})
+
+router.post('/', function(req, res){
+    burger.create(['burger_name','devoured'], [req.body.burger_name, req.body.devoured], function(){
+       res.redirect('/');
+    })
+})
+
+router.put('/:id', function(req,res){
+    var condition = 'id = ' + req.params.id
+
+    console.log('condition', condition)
+
+    burger.update({devoured: req.body.devoured}, condition, function(){
+        res.redirect('/');
+    })
+})
+
+module.exports = router;
