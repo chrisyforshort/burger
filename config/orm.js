2dd2bc22aad2029bc16dff28f8b6cc9c83b46e5f
Christina Wu <CHRISTINASARAHWU@GMAIL.COM>
2018-05-14 16:09:40 -0700

    fixed some code

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,24 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num){
+    var arr = []
+    for(var i = 0; i <num; i++){
+        arr.push('?')
+    }
+    return arr.toString()
+}
+
+function objToSql(ob){
+    var arr = []
+    for(var key in ob){
+        if(Object.hasOwnProperty.call(ob, key)){
+            arr.push(key+ '='+ ob[key])
+        }
+    }
+    return arr.toString()
+}
+
 var orm = {
     // selectAll() //
     selectAll: function() {
@@ -9,7 +27,7 @@ var orm = {
             if (err) {
                 throw err;
             }
-            console.log(res)
+            cb(res)
         })
     },
 
